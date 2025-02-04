996a4d12ef6373fa9ac4afd018aa2127a614063d
peterrexj <peterrexj@hotmail.com>
2025-02-05 01:35:33 +1100

    expander on the disclaimer

--- src/LoanCalculator/Controls/DisclaimerBannerView.xaml.cs
+++ src/LoanCalculator/Controls/DisclaimerBannerView.xaml.cs
@@ -11,6 +11,25 @@ public partial class DisclaimerBannerView : ContentView
         set => SetValue(TextProperty, value);
     }
 
+    public static readonly BindableProperty HeaderTextProperty =
+        BindableProperty.Create(nameof(HeaderText), typeof(string), typeof(DisclaimerBannerView), default(string));
+
+    public string HeaderText
+    {
+        get => (string)GetValue(HeaderTextProperty);
+        set => SetValue(HeaderTextProperty, value);
+    }
+
+
+    public static readonly BindableProperty BannerTypeProperty =
+        BindableProperty.Create(nameof(BannerType), typeof(string), typeof(DisclaimerBannerView), default(string));
+
+    public string BannerType
+    {
+        get => (string)GetValue(BannerTypeProperty);
+        set => SetValue(BannerTypeProperty, value);
+    }
+
     public DisclaimerBannerView()
 	{
 		InitializeComponent();
